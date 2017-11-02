827f7896a2d5b98c060d4a573ce16cb8db547aaf
zk <liuzq7@gmail.com>
2017-11-02 15:53:34 +0800

    增加展示界面 jsp

--- src/main/java/org/smart4j/chapter1/controller/CustomerCreateServlet.java
+++ src/main/java/org/smart4j/chapter1/controller/CustomerCreateServlet.java
@@ -1,16 +1,20 @@
 package org.smart4j.chapter1.controller;
 
+import org.smart4j.chapter1.model.Customer;
+import org.smart4j.chapter1.service.CustomerService;
+
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
+import java.util.List;
 
 /**
  * Created by zk on 01/11/2017.
  */
-@WebServlet("/customer_create")
+@WebServlet("/customer")
 public class CustomerCreateServlet  extends HttpServlet{
 
 //    @Override
@@ -21,6 +25,13 @@ public class CustomerCreateServlet  extends HttpServlet{
 //        req.getRequestDispatcher("/WEB-INF/jsp/hello.jsp").forward(req,resp);
 //    }
 
+    CustomerService customerService;
+
+    @Override
+    public void init() throws ServletException {
+        super.init();
+        customerService=new CustomerService();
+    }
 
     @Override
     protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
@@ -29,6 +40,8 @@ public class CustomerCreateServlet  extends HttpServlet{
 
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-        super.doGet(req, resp);
+        List<Customer> customerList = customerService.getCustomerList();
+        req.setAttribute("customerList",customerList);
+        req.getRequestDispatcher("/WEB-INF/view/customer.jsp").forward(req,resp);
     }
 }
